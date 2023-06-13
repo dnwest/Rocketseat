f4790c51642156bd4079b76ba1051051b551abab
Cristian Fernandes <cristian.daniel.fernandes@gmail.com>
2023-06-12 22:27:13 -0300

    Align margin boxRadio

--- Challenges/Desafio01/ToDo-List/src/components/ListTasks.tsx
+++ Challenges/Desafio01/ToDo-List/src/components/ListTasks.tsx
@@ -1,12 +1,22 @@
 import styles from './ListTasks.module.css'
+import clipBoard from '../assets/Clipboard.svg'
+import trash from '../assets/trash.svg'
 
-export function ListTasks(){
-  return(
-<section>
-<article className={styles.ListTasks}>
-Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
-
-</article>
-</section>
+export function ListTasks() {
+  return (
+    <article>
+      <section className={styles.info}>
+        <img src={clipBoard} />
+        <p><strong>Você ainda não tem tarefas cadastradas</strong><br />
+          Crie tarefas e organize seus itens a fazer</p>
+      </section>
+      <section className={styles.ListTasks}>
+        <div className={styles.boxRadio}>
+        <input type='radio' />
+        Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.
+        <img src={trash} />
+        </div>
+      </section>
+    </article>
   )
 }
--- Challenges/Desafio01/ToDo-List/src/components/Main.tsx
+++ Challenges/Desafio01/ToDo-List/src/components/Main.tsx
@@ -1,20 +1,12 @@
 import styles from './Main.module.css'
-import clipBoard from '../assets/Clipboard.svg'
 
 export function Main(){
   return (
-    <main className={styles.main}>
+    <main>
       <div className={styles.boxInfoTasks}>
       <div className={styles.tasksCreated}>Tarefas criadas <span>0</span></div>
       <div className={styles.done}>Concluídas <span>0</span></div>
       </div> 
-      <section>
-        <article className={styles.info}>
-        <img src={clipBoard}/>
-          <p><strong>Você ainda não tem tarefas cadastradas</strong><br/>
-            Crie tarefas e organize seus itens a fazer</p>
-            </article>
-      </section>
     </main>
   )
 }
