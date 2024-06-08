ef69904bebcba0a7fa14b26b23ed1743b28d143d
Cristian <cristian.daniel.fernandes@gmail.com>
2024-06-07 21:58:00 -0300

    Routes

--- Revision/nodeJS/01-fundamentos-nodejs/src/database.js
+++ Revision/nodeJS/01-fundamentos-nodejs/src/database.js
@@ -28,7 +28,7 @@ this.#database = JSON.parse(data)
         if (Array.isArray(this.#database[table])){
         this.#database[table].push(data)
     } else {
-        this.#database[table] = data
+        this.#database[table] = [data]
     }
 
 this.#persist();
